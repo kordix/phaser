0edf2ea102532c902ee4dd381a4734f33f3c0293
Kordian Bober <BOBKOR@ad.bertrand.pl>
2024-12-08 00:04:12 +0100

    goldmine occupied

--- public/rts/goldIsGathered.js
+++ public/rts/goldIsGathered.js
@@ -2,8 +2,9 @@ function goldIsGathered(player,goldmine,gameinstance){
     if (player.type == 'player' && player.klasa == 'dude') {
         if (Math.abs(player.x - goldmine.x) < 120 && Math.abs(player.y - goldmine.y) < 120) {
             if (player.body.velocity.x == 0) {
-                if (!player.gold) {
+                if (!player.gold && !goldmine.occupied) {
                     goldmine.setTexture('goldmine2');
+                    goldmine.occupied = true;
                     player.gold = true;
                     player.setVelocityX(0);
                     player.setVelocityY(0);
@@ -16,11 +17,12 @@ function goldIsGathered(player,goldmine,gameinstance){
                     setTimeout(() => {
                         player.setVisible(true);
                         goldmine.setTexture('goldmine');
+                        goldmine.occupied = false;
                         console.log('MOVE TO OBJECT');
                         vector.x = 200;
                         vector.y = 500;
                         gameinstance.physics.moveToObject(player, vector, player.speed);
-                    }, 3500)
+                    }, 1000)
                 }
             }
         }
